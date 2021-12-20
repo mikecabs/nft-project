2942c7a7175cc62c16e2a818fcd0e89da6e38219
Jared Spickard <jaredspickard@berkeley.edu>
2021-12-19 16:27:07 -0800

    deleted initial implementation to replace with stuff I found from a tutorial related to metamask

---
+++ web/react-fe/src/App.tsx
@@ -0,0 +1,22 @@
+// App.tsx
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
+import theme from "./theme";
+import Layout from "./components/Layout"
+import ConnectButton from "./components/ConnectButton";
+import AccountModal from "./components/AccountModal";
+import "@fontsource/inter";
+
+function App() {
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	return (
+		// lets us use Chakra UI syntax across our app:
+		<ChakraProvider theme={theme}>
+			<Layout>
+				<ConnectButton handleOpenModal={onOpen} />
+				<AccountModal isOpen={isOpen} onClose={onClose} />
+			</Layout>
+		</ChakraProvider>
+	)
+}
+
+export default App;
---
+++ web/react-fe/src/index.tsx
@@ -0,0 +1,16 @@
+// index.tsx
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
+// Import DAppProvider
+import { DAppProvider } from "@usedapp/core"
+
+ReactDOM.render(
+	<React.StrictMode>
+		{}
+		<DAppProvider config={{}}>
+			<App />
+		</DAppProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
